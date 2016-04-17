8682e052af9f15de49d01e581a83d9bea8369063
webttp <smartin.black@gmail.com>
2016-04-17 18:22:45 +0530

    ghjgj

---
+++ about-us.php
@@ -0,0 +1,82 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CFC CHENNAI CHURCH</title>
+<meta name="keywords" content="free css templates, church website, CSS, HTML" />
+<meta name="description" content="Church website layout - free css template provided by templatemo.com" />
+<link href="styles/templatemo_style.css" rel="stylesheet" type="text/css" />
+<!-- Free CSS Templates from TemplateMo.com -->
+</head>
+<body>
+<div id="templatemo_container">
+	<div id="templatemo_header">
+        <div id="out_mission_section">
+            <p>
+           That all human beings are dead in sin and utterly lost and that the only way their sins can be forgiven is through repentance and through faith in the death and resurrection of our Lord Jesus Christ. <a href="subpage.php">more...</a>
+            </p>
+        </div>
+        <div id="daily_bible_verse_section">
+        	<p> Do to others as you would have them do to you. </p>
+        	<div id="bible_verse">Luke 6:31</div>
+        </div>
+    
+    </div> <!-- end of header -->
+    
+    <div id="templatemo_menu">
+         <ul>
+            <li><a href="index.php" class="current">Home</a></li>
+            <li><a href="about-us.php" >About Us</a></li>
+            <li><a href="media.php">Media</a></li>
+            <li><a href="about-zac.php">About Zac Poonen</a></li>  
+            <li><a href="locate-us.php">Locate Us</a></li> 
+                            
+        </ul> 
+    </div> <!-- end of menu -->
+    
+    <div id="templatemo_content">
+    	<div id="templatemo_left">
+        	<div id="templatemo_news_section">
+            	<h1>Our Meetings</h1>
+                 <div class="templatemo_news_box">
+                    <h2>CFC -Thirumullaivoyal</h2>
+                    <h3>Sunday : 10.30 AM - 1.00 PM</h3>
+                    <p>
+                    For Directions
+    <a href="locate-us.php">[Click Here]</a></p>
+				</div>
+                <div class="templatemo_news_box">
+                    <h2>CFC -Tambaram</h2>
+                    <h3>Sunday : 10.30 AM - 1.00 PM</h3>
+					<h3>Thursday : 7.30 PM - 8.30 PM</h3>
+					<p>
+                    For Directions
+    <a href="locate-us.php">[Click Here]</a></p>
+				</div>
+            </div>
+           
+        </div> <!-- end of left -->
+        
+        <div id="templatemo_right">
+        
+       	  <div class="right_col_section">
+   	      <h1>About-Us</h1>
+                <p><img src="images/templatemo_image_01.jpg" alt="church" />The church of believers that meets at Chennai Christian Fellowship Church (CCFC) today commenced with a few families that first met together in Chennai, India.</p>
+                
+              <p>First be disciples of the Lord Jesus Christ themselves, and then to make disciples in obedience to the Lord's commission in Matthew 28:18-20.</p>
+            <p>We emphasized new birth, holiness of inner life, mutual love, moral purity, financial integrity and sharing God's truth with others -founding every aspect of their lives on God's Word alone.</p>
+</div>
+            <div class="cleaner_with_height">&nbsp;</div>
+            
+            
+            <div class="cleaner">&nbsp;</div>
+        </div> <!-- end of right -->
+    </div> <!-- end of content -->
+    
+    <div id="templatemo_footer">
+    	Copyright © 2016 <a href="http://www.cfcchennai.herokuapp.com">The Church</a> 
+    </div> <!-- end of footer -->
+</div> <!-- end of container -->
+</body>
+</html>
