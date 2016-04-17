75a79743451997df2c16be20f20afdad0221a247
webttp <smartin.black@gmail.com>
2016-04-17 21:29:24 +0530

    db table connect

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         $password = "aom0shz4do0uf1bi";
         
         // Create connection
-        $conn = new mysqli($servername, $username, $password);
+        $conn = new mysqli($servername, $username, $password,$dbname = "rrbnvpvpvc79aytz");
         
         // Check connection
         if ($conn->connect_error) {
