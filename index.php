11765703ba634dd20e69cae6aa51ff0bbd04aec7
webttp <smartin.black@gmail.com>
2016-04-20 10:13:22 +0530

    we are coming soon

--- index.php
+++ index.php
@@ -30,14 +30,14 @@
     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="col-md-1 col-sm-1 col-xs-1 cfcc-logo">
-          <a href="http://www.cfcindia.com/"> <img alt="Logo"
+          <a href="#"> <img alt="Logo"
             class="img-responsive main-logo" src="images/Cfc_Chennai_Logo.png"
             alt="Chennai-CFC">
           </a>
         </div>
         <div class="col-md-10 col-sm-11 col-xs-10  cfcc-verse">
           <div class="col-md-9 col-sm-10 col-xs-9">
-            <a href="http://www.cfcindia.com">
+            <a href="#">
               <div class="hidden-xs banner-text">Christian Fellowship Church, Chennai</div>
             </a>
             <div class="hidden-xs verse-text">
@@ -45,7 +45,7 @@
             </div>
           </div>
           <div class="col-md-1 col-md-offset-1 col-sm-1 hidden-xs">
-            <a class="fb-share-button" href="https://www.facebook.com/groups/1419135528352392/"></a>
+            <a class="fb-share-button" href="#"></a>
           </div>
         </div>
       </div>
@@ -67,12 +67,7 @@
               <span class="icon-bar"></span>
             </button>
           </div>
-          <div id="menu_items_bar" class="collapse navbar-collapse">
-            <ul class="nav navbar-main">
-              <li class='active' ><a href='#' title="home" class='home' style="padding-left:50px"><span >We are Coming Soon</span></a></li>
-             
-            </ul>
-          </div>
+          
         </nav>
       </div>
     </div>
@@ -82,19 +77,7 @@
 	<div class="home-page-section">
 	   	<div data-include="templates/home.php"></div>
 	</div>
-	<div class="content-page-section" style="display:none;">
-		<div class="container body-container bs-docs-container">
-			<div class="row-fluid" id="full-width">
-				<div class="col-md-12 home-page">
-					<div class="col-md-8 content-section" id="content_section">
-					</div>
-					<div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
-					   <div data-include="templates/timings.php"></div>
-					</div>
-        </div>
-			</div>
-		</div>
-	</div>
+	
 	<footer>
 	<div id="footer_container" class="footer-container">
 		<div class="footer-menu">
