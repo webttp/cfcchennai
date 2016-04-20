7757b463d3c087c8b62823df6665bc091c9f8d6c
webttp <smartin.black@gmail.com>
2016-04-20 08:14:23 +0530

    dsada

--- index.php
+++ index.php
@@ -1,82 +1,135 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CFC CHENNAI CHURCH</title>
-<meta name="keywords" content="free css templates, church website, CSS, HTML" />
-<meta name="description" content="Church website layout - free css template provided by templatemo.com" />
-<link href="styles/templatemo_style.css" rel="stylesheet" type="text/css" />
-<!-- Free CSS Templates from TemplateMo.com -->
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>CFC CHENNAI</title>
+<!-- Script files -->
+<script type="text/javascript" src="js/jQuery.js"></script>
+<script type="text/javascript" src="js/siteProgramming.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/csi.min.js"></script>
+
+<!-- CSS files  -->
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/slider.css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<style>
+  @import url("bootstrap/css/bootstrap.min.css");
+  @import url("css/slider.css");
+</style>
+
 </head>
-<body>
-<div id="templatemo_container">
-	<div id="templatemo_header">
-        <div id="out_mission_section"> 
-            <p>
-           That all human beings are dead in sin and utterly lost and that the only way their sins can be forgiven is through repentance and through faith in the death and resurrection of our Lord Jesus Christ. <a href="subpage.php">more...</a>
-            </p>
+
+<body class="no-sidebars md"> 
+   <header>
+   		<div class="menu-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="col-md-1 col-sm-1 col-xs-1 cfcc-logo">
+          <a href="http://www.cfcindia.com/"> <img alt="Logo"
+            class="img-responsive main-logo" src="images/Cfc_Chennai_Logo.png"
+            alt="Chennai-CFC">
+          </a>
         </div>
-        <div id="daily_bible_verse_section">
-        	<p> Do to others as you would have them do to you. </p>
-        	<div id="bible_verse">Luke 6:31</div>
-        </div>
-    
-    </div> <!-- end of header -->
-    
-    <div id="templatemo_menu">
-        <ul>
-            <li><a href="index.php" class="current">Home</a></li>
-            <li><a href="#" >About Us</a></li>
-            <li><a href="#">Media</a></li>
-            <li><a href="#">About Zac Poonen</a></li>  
-            <li><a href="#">Locate Us</a></li> 
-                            
-        </ul>  
-    </div> <!-- end of menu -->
-    
-    <div id="templatemo_content">
-    	<div id="templatemo_left">
-        	<div id="templatemo_news_section">
-            	<h1>Our Meetings</h1>
-                 <div class="templatemo_news_box">
-                    <h2>CFC -Thirumullaivoyal</h2>
-                    <h3>Sunday : 10.30 AM - 1.00 PM</h3>
-                    <p>
-                    For Directions
-    <a href="#">[Click Here]</a></p>
-				</div>
-                <div class="templatemo_news_box">
-                    <h2>CFC -Tambaram</h2>
-                    <h3>Sunday : 10.30 AM - 1.00 PM</h3>
-					<h3>Thursday : 7.30 PM - 8.30 PM</h3>
-					<p>
-                    For Directions
-    <a href="#">[Click Here]</a></p>
-				</div>
+        <div class="col-md-10 col-sm-11 col-xs-10  cfcc-verse">
+          <div class="col-md-9 col-sm-10 col-xs-9">
+            <a href="http://www.cfcindia.com">
+              <div class="hidden-xs banner-text">Christian Fellowship Church, Chennai</div>
+            </a>
+            <div class="hidden-xs verse-text">
+              <em>And he who does not take his cross and follow after Me is not worthy of Me </br> (Matthew 10:38) </em>
             </div>
-           
-        </div> <!-- end of left -->
-        
-        <div id="templatemo_right">
-        
-       	  <div class="right_col_section">
-   	      <h1>What We believe</h1>
-                <p><img src="images/templatemo_image_01.jpg" alt="church" />That all human beings are dead in sin and utterly lost and that the only way their sins can be forgiven is through repentance and through faith in the death and resurrection of our Lord Jesus Christ.</p>
-                
-              <p>That there is one God eternally existent in three Persons: Father, Son and Holy Spirit.</p>
-            <p>In the regenerating work of the Holy Spirit, whereby a person is born again to be a child of God.</p>
+          </div>
+          <div class="col-md-1 col-md-offset-1 col-sm-1 hidden-xs">
+            <a class="fb-share-button" href="https://www.facebook.com/groups/1419135528352392/"></a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  </div>
+<div class="menu-nav-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
+          <div class="hidden-md hidden-lg hidden-sm col-xs-10 xs-banner-text">
+            Christian Fellowship Church, Chennai</div>
+          <div class="navbar-header toggle-bar">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu_items_bar">
+              <span class="icon-bar"></span> 
+              <span class="icon-bar"></span> 
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div id="menu_items_bar" class="collapse navbar-collapse">
+            <ul class="nav navbar-main">
+              <li class='active' ><a href='#' title="home" class='home' style="padding-left:50px"><span >We are Coming Soon</span></a></li>
+             
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
 </div>
-            <div class="cleaner_with_height">&nbsp;</div>
-            
-            
-            <div class="cleaner">&nbsp;</div>
-        </div> <!-- end of right -->
-    </div> <!-- end of content -->
-    
-    <div id="templatemo_footer">
-    	Copyright © 2016 <a href="http://www.cfcchennai.herokuapp.com">The Church</a> 
-    </div> <!-- end of footer -->
-</div> <!-- end of container -->
+</header>
+	<div class="home-page-section">
+	   	<div data-include="templates/home.html"></div>
+	</div>
+	<div class="content-page-section" style="display:none;">
+		<div class="container body-container bs-docs-container">
+			<div class="row-fluid" id="full-width">
+				<div class="col-md-12 home-page">
+					<div class="col-md-8 content-section" id="content_section">
+					</div>
+					<div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
+					   <div data-include="templates/timings.html"></div>
+					</div>
+        </div>
+			</div>
+		</div>
+	</div>
+	<footer>
+	<div id="footer_container" class="footer-container">
+		<div class="footer-menu">
+			<div class="container footer-container">
+				<div class="col-md-4 col-sm-4 hidden-xs footer-section-1">
+					<h4>CFC Thirumullaivoyal address</h4>
+					<ul>
+						<li>Plot No. 9 Saraswathi Nagar</li>
+						<li>A.I.R Transmission Towers Compound</li>
+						<li>Thirumullaivoyal</li>
+						<li>Chennai - 600 066</li>
+						<li>Ph : 9444914400, 9840154586</li>
+					</ul>
+				</div>
+				<div class="col-md-4 col-sm-4 hidden-xs footer-section-2">
+					<h4>CFC Tambaram address</h4>
+					<ul>
+						<li>5/58 First Floor, Ganesh Nagar Main Road</li>
+						<li>Selaiyur Post</li>
+						<li>Chennai - 600 073</li>
+						<li>Ph : 9789019443, 9444119988</li>
+					</ul>
+				</div>
+				<div class="col-md-4 col-sm-4 hidden-xs footer-section-3">
+					<h4>Recommended Links</h4>
+					<ul>
+						<li><a href="http://www.cfcindia.com">www.cfcindia.com</a></li>
+						<li><a href="http://www.rlcf.org">www.rlcf.org</a></li>
+						<li><a href="http://www.nccf.com">www.nccf.com</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="cfcc-bottom-footer">All rights reserved @ cfcchennai.church 2016</div>
+		</div>
+  </div>
+</footer>
 </body>
 </html>
