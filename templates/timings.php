099ad2eff7793ed7b50549b660bfcd59f86bbc75
webttp <smartin.black@gmail.com>
2016-04-23 00:37:53 +0530

    Timings file changed

--- templates/timings.php
+++ templates/timings.php
@@ -2,23 +2,25 @@
 	<h3>OUR MEETINGS</h3>
 </div>
 <div class="col-md-12 col-sm-6 col-xs-12  nopadding meeting-timings">
-	<div class="cfcc-meetings-place">
+	<div class="cfcc-meetings-place thirumullai-cfc">
 		<a href="http://www.cfcindia.com/locate-us"> CFC Thirumullaivoyal</a>
 	</div>
 	<div class="cfcc-timings">SUNDAY : 10:30AM - 12:30PM</div>
+	<div class="col-md-12 col-sm-6 col-xs-12 nopadding direction">
+		<a class="directions cfcc-thirumullai-directions" href="#"> </a>
+	</div>
 </div>
-<div class="col-md-12 col-sm-6 col-xs-12 nopadding direction">
-  <a class="cfcc-directions" href="#"> </a>
-</div>
+
 <div class="col-md-12 col-sm-6 col-xs-12 nopadding meeting-timings">
-	<div class="cfcc-meetings-place">
+	<div class="cfcc-meetings-place tambaram-cfc">
 		<a href="http://www.cfcindia.com/locate-us"> CFC Tambaram</a>
 	</div>
 	<div class="cfc-timings">SUNDAY : 10:30AM - 12:30PM</div>
 	<div class="cfc-timings">THURSDAY : 7:30PM - 8:30PM</div>
+	<div class="col-md-12 col-sm-6 col-xs-12 nopadding direction">
+		<a class="directions cfcc-tambaram-directions" href="#"> </a>
+	</div>
 </div>
-<div class="col-md-12 col-sm-6 col-xs-12 nopadding direction">
-	<a class="cfcc-directions" href="#"> </a>
-</div>
+
 
 
