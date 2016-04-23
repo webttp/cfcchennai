aa6af49e32b772586695f88c325d5498469ea999
webttp <smartin.black@gmail.com>
2016-04-23 23:23:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/csi.min.js"></script>
 <script type="text/javascript" src="js/angular.min.js"></script>
+<script type="text/javascript" src="js/sermonController.js"></script>
 <script type="text/javascript" src="js/lightbox.js"></script>
 
   <!-- CSS files  -->
@@ -29,17 +30,55 @@
 
 <body class="no-sidebars md"> 
    <header>
+	<div class="menu-container">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1 col-sm-1 col-xs-1 cfcc-logo">
+					<a href="http://www.cfcindia.com/"> <img alt="Logo" class="img-responsive main-logo" src="images/Cfc_Chennai_Logo.png" alt="Chennai-CFC">
+					</a>
+				</div>
+				<div class="col-md-11 col-sm-11 col-xs-11 cfcc-verse">
+					<div class="col-md-10 col-sm-10 hidden-xs">
+						<a href="http://www.cfcindia.com">
+							<div class="hidden-xs banner-text">Christian Fellowship
+								Church, Chennai</div>
+						</a>
+						<!-- <div class="hidden-xs verse-text">
+                      <em>And he who does not take his cross and follow after Me is not worthy of Me </br> (Matthew 10:38) </em>
+                  </div> -->
+					</div>
+					<div class="hidden-md hidden-lg hidden-sm col-xs-11">
+						<nav class="navbar navbar-default navbar-inverse menu" role="navigation">
+						<div class="hidden-md hidden-lg hidden-sm col-xs-10 xs-banner-text">
+							 Christian Fellowship Church, Chennai</div>
+						<div class="navbar-header toggle-bar">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu_items_bar">
+								<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+							</button>
+						</div>
+				    
+						</nav>
+					</div>
+					<div class="col-md-1 col-sm-1 col-xs-1">
+						<a class="fb-share-button"
+							href="https://www.facebook.com/groups/1419135528352392/"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </header>
 <div class="spinner-container" style="display:none;">
 
 </div>
-	<div class="content-page-section" style="display:none;">
+	
+	<div class="content-page-section">
 		<div class="container-fluid body-container bs-docs-container">
 			<div class="row-fluid" id="full-width">
 				<div class="col-md-12 home-page">
 					<div class="col-md-8 content-section" id="content_section">
-						We are coming soon ....
+						We are Coming Soon ..
 					</div>
 					<div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
 					   <div data-include="templates/timings.php"></div>
@@ -52,38 +91,41 @@
 	<div id="footer_container" class="footer-container">
 		<div class="footer-menu">
 			<div class="container footer-area">
-				<div class="col-md-4 col-sm-4 col-xs-12 footer-section-1">
-					<h4>CFC Thirumullaivoyal address</h4>
-					<ul>
-						<li>Plot No. 9 Saraswathi Nagar</li>
-						<li>A.I.R Transmission Towers Compound</li>
-						<li>Thirumullaivoyal</li>
-						<li>Chennai - 600 066</li>
-						<li>Ph : 9710925425, 9840154586</li>
-					</ul>
+				<div class="row">
+					<div class="col-md-4 col-sm-4 col-xs-12 footer-section-1">
+						<h4>CFC Thirumullaivoyal address</h4>
+						<ul>
+							<li>Plot No. 9 Saraswathi Nagar</li>
+							<li>A.I.R Transmission Towers Compound</li>
+							<li>Thirumullaivoyal</li>
+							<li>Chennai - 600 066</li>
+							<li>Ph : 9444914400, 9840154586</li>
+						</ul>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12 footer-section-2">
+						<h4>CFC Tambaram address</h4>
+						<ul>
+							<li>5/58 First Floor, Ganesh Nagar Main Road</li>
+							<li>Selaiyur Post</li>
+							<li>Chennai - 600 073</li>
+							<li>Ph : 9789019443, 9444119988</li>
+						</ul>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12 footer-section-3">
+						<h4>Recommended Links</h4>
+						<ul>
+							<li><a href="http://www.cfcindia.com">www.cfcindia.com</a></li>
+							<li><a href="http://www.rlcf.org">www.rlcf.org</a></li>
+							<li><a href="http://www.nccf.com">www.nccf.com</a></li>
+						</ul>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-4 col-xs-12 footer-section-2">
-					<h4>CFC Tambaram address</h4>
-					<ul>
-						<li>5/58 First Floor, Ganesh Nagar Main Road</li>
-						<li>Selaiyur Post</li>
-						<li>Chennai - 600 073</li>
-						<li>Ph : 9789019443, 9444119988</li>
-					</ul>
-				</div>
-				<div class="col-md-4 col-sm-4 col-xs-12 footer-section-3">
-					<h4>Recommended Links</h4>
-					<ul>
-						<li><a href="http://www.cfcindia.com">www.cfcindia.com</a></li>
-						<li><a href="http://www.rlcf.org">www.rlcf.org</a></li>
-						<li><a href="http://www.nccf.com">www.nccf.com</a></li>
-					</ul>
-				</div>
+				<div class="cfcc-bottom-footer">All rights reserved @
+					cfcchennai.church 2016</div>
 			</div>
-			<div class="cfcc-bottom-footer">All rights reserved @ cfcchennai.church 2016</div>
 		</div>
-  </div>
-</footer>
+	</div>
+	</footer>
 <script src="js/lightbox.js"></script>
 </body>
 </html>
