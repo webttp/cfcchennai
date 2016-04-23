2e5daa48a4ea63161fdf7f8a825ff9b56f429007
webttp <smartin.black@gmail.com>
2016-04-23 23:09:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,88 +29,17 @@
 
 <body class="no-sidebars md"> 
    <header>
-  <div class="menu-container">
-  <div class="container">
-    <div class="row">
-					<div class="col-md-1 col-sm-1 col-xs-1 cfcc-logo">
-						<a href="http://www.cfcindia.com/"> 
-						 <img alt="Logo" class="img-responsive main-logo" src="images/Cfc_Chennai_Logo.png" alt="Chennai-CFC">
-						</a>
-					</div>
-					<div class="col-md-11 col-sm-11 col-xs-11 cfcc-verse">
-						<div class="col-md-10 col-sm-10 hidden-xs">
-							<a href="http://www.cfcindia.com">
-								<div class="hidden-xs banner-text"> Christian Fellowship Church, Chennai</div>
-							</a>
-							<!-- <div class="hidden-xs verse-text">
-                      <em>And he who does not take his cross and follow after Me is not worthy of Me </br> (Matthew 10:38) </em>
-                  </div> -->
-						</div>
-						<div class="hidden-md hidden-lg hidden-sm col-xs-11">
-						  <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
-								<div class="hidden-md hidden-lg hidden-sm col-xs-10 xs-banner-text">
-	                  Christian Fellowship Church, Chennai
-	              </div>
-								<div class="navbar-header toggle-bar">
-									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu_items_bar">
-										<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
-									</button>
-								</div>
-							</nav>
-						</div>
-						<div class="col-md-1 col-sm-1 col-xs-1">
-              <a class="fb-share-button" href="https://www.facebook.com/groups/1419135528352392/"></a>
-            </div>
-					</div>
-				</div>
-  </div>
-  </div>
-<div class="menu-nav-container">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
-          <div id="menu_items_bar" class="collapse navbar-collapse">
-            <ul class="nav navbar-main">
-              <li class='active dropdown parent_menu'>
-                <a href='#' title="home" class='home dropdown-toggle' data-toggle='dropdown'><span>Home</span></a>
-              </li>
-              <li class='dropdown parent_menu'>
-                <a href="#" title="aboutUs" class='aboutUs dropdown-toggle' data-toggle='dropdown'><span>About us</span></a>
-              </li>
-              <li class='dropdown parent_menu'>
-                <a href='#' title="sermons"  class='sermons dropdown-toggle' data-toggle='dropdown'><span>Sermons</span></a>
-              </li>
-              <li class='dropdown parent_menu'>
-                <a href='#' title="aboutBrother"  class='aboutBrother dropdown-toggle' data-toggle='dropdown'><span>About Zac Poonen</span></a>
-              </li>
-              <li class="has-sub dropdown parent_menu">
-                <a href='#' title="locateUs"  class='locateUs dropdown-toggle' data-toggle='dropdown'><span>Locate Us</span>
-                  <i class="fa fa-caret-down" aria-hidden="true"></i></a>
-                  <ul class="dropdown-menu  multi-column-dropdown">
-                    <li class="child_menu menu-list-item"><a><span> CFC Thirumullaivoyal</span></a></li>
-                    <li class="child_menu menu-list-item"><a><span> CFC Tambaram </span></a></li>
-                  </ul>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </div>
-  </div>
-</div>
+
 </header>
 <div class="spinner-container" style="display:none;">
 
 </div>
-	<div class="home-page-section">
-	   	<div data-include="templates/home.php"></div>
-	</div>
 	<div class="content-page-section" style="display:none;">
 		<div class="container-fluid body-container bs-docs-container">
 			<div class="row-fluid" id="full-width">
 				<div class="col-md-12 home-page">
 					<div class="col-md-8 content-section" id="content_section">
+						We are coming soon ....
 					</div>
 					<div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
 					   <div data-include="templates/timings.php"></div>
