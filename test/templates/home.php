c94213453bb93c906c2c96bda75405851b238970
webttp <smartin.black@gmail.com>
2016-04-25 13:10:31 +0530

    Update home.php

--- test/templates/home.php
+++ test/templates/home.php
@@ -116,18 +116,17 @@
 							<div class="meeting-heading">
 								<h3>OUR MEETINGS</h3>
 							</div>
-							<div
-								class="col-md-12 col-sm-6 col-xs-12  nopadding meeting-timings">
+							<div class="col-md-12 col-sm-6 col-xs-12  nopadding meeting-timings">
 								<div class="cfcc-meetings-place thirumullai-cfc">
-									<a href="http://www.cfcindia.com/locate-us"> CFC Thirumullaivoyal</a>
+									<a href="#"> CFC Thirumullaivoyal</a>
 								</div>
 								<div class="cfcc-timings">SUNDAY:10:30AM - 12:30PM</div>
-								<a class="col-md-12 col-sm-6 col-xs-12 directions cfcc-thirumullai-directions" href="#">
-								</div>
+								<a class="col-md-12 col-sm-6 col-xs-12 directions cfcc-thirumullai-directions" href="#"></a>
+								
 							</div>
 							<div class="col-md-12 col-sm-6 col-xs-12 nopadding meeting-timings">
 								<div class="cfcc-meetings-place tambaram-cfc">
-									<a href="http://www.cfcindia.com/locate-us"> CFC Tambaram</a>
+									<a href="#"> CFC Tambaram</a>
 								</div>
 								<div class="cfcc-timings">SUNDAY : 10:30AM - 12:30PM</div>
 								<div class="cfcc-timings">THURSDAY : 7:30PM - 8:30PM</div>
