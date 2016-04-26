7d0f6426203c6cbcdc8d3438039e5f9e55d35928
webttp <smartin.black@gmail.com>
2016-04-27 00:26:16 +0530

    Update aboutBrother.php

--- test/templates/aboutBrother.php
+++ test/templates/aboutBrother.php
@@ -1,31 +1,93 @@
-<h4>About Zac Poonen</h4>
-<div class="region region-content">
-	<div class="content">
-		<div class="field-items">
-			<figure class="inner-image pull-left">
-					<a class="inner-image-link" href="http://www.cfcindia.com/resources/en/images/about-zac-poonen.jpg"
-						data-lightbox="inner-iamge-1" data-title="Zac Poonen"> 
-						<img src="http://www.cfcindia.com/resources/en/images/about-zacpoonen.jpg" alt="Zac Poonen"><br>
-						<figcaption>Zac Poonen</figcaption></a>
-				</figure>
-				<p>Zac Poonen was formerly an Indian Naval Officer who has been
-					serving the Lord in India for over 50 years as a Bible-teacher. He
-					has responsibility for a number of churches in India and abroad.</p>
-				<p>He has written more than 25 books and numerous articles in
-					English - which have been translated into many Indian and foreign
-					languages. His messages are available on audio CDs and video DVDs.</p>
-				<p>
-					Like the other elders in CFC, Zac Poonen also supports himself and
-					his family through "
-					<a href="http://www.cfcindia.com/books/the-day-of-small-beginnings#Money-And-Gods-Work">tent-making</a>"
-					and does not receive any salary for his services. He does not
-					receive any royalty for any of his books, CDs or DVDs that are
-					published by Christian Fellowship Center, Bangalore.
-				</p>
-				<p> For Zac Poonen's personal testimony, go to the following links:<br>
-					<a href="http://www.cfcindia.com/books/the-day-of-small-beginnings">The
-						Day Of Small Beginnings </a><br>
-				</p>
-		</div>
-	</div>
-</div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+      <meta name="description" content="A Church of disciples of the Lord Jesus Christ, at Chennai since 1986, 
+         emphasizing new birth, holiness of inner life, mutual love, financial integrity and sharing God's truth with others. 
+         An evangelical, non-denominational church in Chennai, India.">
+      <meta name="keywords" content="Chennai,Zac Poonen, Zac poonen church in Chennai, cfc church in chennai, Chennai church,church chennai,christian chennai,India,church india,church south india,
+         churches in india,churches in chennai, church in india,christian,chrisrtian india,fellowship,Christian fellowship Church in Chennai, cfc chennai, cfc in chennai,cfc">
+      <meta name="MobileOptimized" content="width">
+      <meta name="HandheldFriendly" content="true">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="cleartype" content="on">
+      <title>CHENNAI CHURCH | Christian Fellowship Church, India </title>
+      <!-- lightbox css files -->
+      <link rel="stylesheet" type="text/css" href="css/lightbox.css">
+      <!-- Script files -->
+      <script type="text/javascript" src="js/jQuery.js"></script>
+      <script type="text/javascript" src="js/siteProgramming.js"></script>
+      <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+      <script type="text/javascript" src="js/csi.min.js"></script>
+      <script type="text/javascript" src="js/angular.min.js"></script>
+      <script type="text/javascript" src="js/sermonController.js"></script>
+      <script type="text/javascript" src="js/lightbox.js"></script>
+      <!-- CSS files  -->
+      <link rel="stylesheet" type="text/css" href="css/styles.css">
+      <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css">
+      <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="css/slider.css">
+      <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+      <style>
+         @import url("bootstrap/css/bootstrap.min.css");
+         @import url("css/slider.css");
+      </style>
+   </head>
+   <body class="no-sidebars md">
+      <header>
+         <div data-include="templates/menuHeader.php"></div>
+      </header>
+      <div class="spinner-container" style="display:none;"></div>
+      <div class="content-page-section" style="display:none;">
+         <div class="container body-container bs-docs-container">
+            <div class="row-fluid" id="full-width">
+               <div class="col-md-12 home-page">
+                  <div class="col-md-8 content-section" id="content_section">
+                     <h4>About Zac Poonen</h4>
+                     <div class="region region-content">
+                        <div class="content">
+                           <div class="field-items">
+                              <figure class="inner-image pull-left">
+                                 <a class="inner-image-link" href="http://www.cfcindia.com/resources/en/images/about-zac-poonen.jpg"
+                                    data-lightbox="inner-iamge-1" data-title="Zac Poonen">
+                                    <img src="http://www.cfcindia.com/resources/en/images/about-zacpoonen.jpg" alt="Zac Poonen"><br>
+                                    <figcaption>Zac Poonen</figcaption>
+                                 </a>
+                              </figure>
+                              <p>Zac Poonen was formerly an Indian Naval Officer who has been
+                                 serving the Lord in India for over 50 years as a Bible-teacher. He
+                                 has responsibility for a number of churches in India and abroad.
+                              </p>
+                              <p>He has written more than 25 books and numerous articles in
+                                 English - which have been translated into many Indian and foreign
+                                 languages. His messages are available on audio CDs and video DVDs.
+                              </p>
+                              <p>
+                                 Like the other elders in CFC, Zac Poonen also supports himself and
+                                 his family through "
+                                 <a href="http://www.cfcindia.com/books/the-day-of-small-beginnings#Money-And-Gods-Work">tent-making</a>"
+                                 and does not receive any salary for his services. He does not
+                                 receive any royalty for any of his books, CDs or DVDs that are
+                                 published by Christian Fellowship Center, Bangalore.
+                              </p>
+                              <p> For Zac Poonen's personal testimony, go to the following links:<br>
+                                 <a href="http://www.cfcindia.com/books/the-day-of-small-beginnings">The
+                                 Day Of Small Beginnings </a><br>
+                              </p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
+                     <div data-include="templates/timings.php"></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <footer>
+         <div data-include="templates/footer.php"></div>
+      </footer>
+      <script src="js/lightbox.js"></script>
+   </body>
+</html>
