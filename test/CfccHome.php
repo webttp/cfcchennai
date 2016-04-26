aaa671d35a0b214a10bdec48511e1dae27aba35a
webttp <smartin.black@gmail.com>
2016-04-26 18:58:02 +0530

    Update CfccHome.php

--- test/CfccHome.php
+++ test/CfccHome.php
@@ -39,84 +39,86 @@ churches in india,churches in chennai, church in india,christian,chrisrtian indi
 
 <body class="no-sidebars md"> 
    <header>
-	<div class="menu-container">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-1 col-sm-1 col-xs-1 cfcc-logo">
-					<a href="http://www.cfcindia.com/"> 
-					<img class="img-responsive main-logo" src="images/Logo.png" alt="Chennai-CFC-Image">
-					</a>
-				</div>
-				<div class="col-md-11 col-sm-11 col-xs-11 cfcc-verse">
-					<div class="col-md-10 col-sm-10 hidden-xs">
-						<a href="http://www.cfcindia.com">
-							<div class="hidden-xs banner-text">Christian Fellowship
-								Church, Chennai</div>
-						</a>
-						<!-- <div class="hidden-xs verse-text">
+  <div class="menu-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-1 col-sm-1 col-xs-2 cfcc-logo">
+          <a href="http://www.cfcindia.com/"> 
+          <img class="img-responsive main-logo" src="images/Logo.png" alt="Chennai-CFC-Image">
+          </a>
+        </div>
+        <div class="col-md-10 col-sm-10 col-xs-8 cfcc-verse">
+          <div class="col-md-10 col-sm-10 hidden-xs">
+            <a href="http://www.cfcindia.com">
+              <div class="hidden-xs banner-text">Christian Fellowship
+                Church, Chennai</div>
+            </a>
+            <!-- <div class="hidden-xs verse-text">
                       <em>And he who does not take his cross and follow after Me is not worthy of Me </br> (Matthew 10:38) </em>
                   </div> -->
-					</div>
-					<div class="hidden-md hidden-lg hidden-sm col-xs-10">
-						<nav class="navbar navbar-default navbar-inverse menu" role="navigation">
-						<div class="hidden-md hidden-lg hidden-sm col-xs-10 xs-banner-text">
-							 Christian Fellowship Church, Chennai</div>
-						<div class="navbar-header toggle-bar">
-							<button type="button" id="menu_btn" class="navbar-toggle" data-toggle="collapse" data-target="#menu_items_bar">
-								<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
-							</button>
-						</div>
-				    	<div id="menu_items_bar" class="overlay-container collapse navbar-collapse">
-						<label for="menu_btn"></label>
-							<ul class="nav navbar-main">
-								<li class='active dropdown parent_menu'>
-								  <a href='#' title="home" class='home dropdown-toggle' data-toggle='dropdown'>
-								  <span>Home</span></a>
-								</li>
-								<li class='dropdown parent_menu'>
-								  <a href="#" title="aboutUs" class='aboutUs dropdown-toggle' data-toggle='dropdown'>
-								  <span>About us</span></a>
-								</li>
-								<li class='dropdown parent_menu'>
-								  <a href='#' title="sermons" class='sermons dropdown-toggle' data-toggle='dropdown'>
-								  <span>Sermons</span></a>
-								</li>
-								<li class='dropdown parent_menu'>
-								  <a href='#' title="aboutBrother" class='aboutBrother dropdown-toggle' data-toggle='dropdown'>
-								  <span>About Zac Poonen</span></a>
-								</li>
-								<li class="has-sub dropdown parent_menu">
-								  <a href='#' title="locateUs" class='locateUs dropdown-toggle' data-toggle='dropdown'>
-								  <span>Locate Us</span> <i class="fa fa-caret-down" aria-hidden="true"></i></a>
-									<ul class="dropdown-menu  multi-column-dropdown">
-										<li class="child_menu menu-list-item">
-										  <a><span>CFC Thirumullaivoyal</span></a>
-										</li>
-										<li class="child_menu menu-list-item">
-										  <a><span> CFC Tambaram </span></a>
-										</li>
-									</ul>
-								</li>
-							</ul>
-						</div>
-						</nav>
-					</div>
-					<div class="col-md-1 col-sm-1 col-xs-1">
-							<a class="fb-share-button"
-								href="https://www.facebook.com/groups/1419135528352392/"></a>
-					</div>
-					
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="menu-nav-container">
+          </div>
+          <div class="hidden-md hidden-lg hidden-sm col-xs-12 xs-banner-text">
+               Christian Fellowship Church, Chennai</div>
+        </div>
+        <div class="col-md-1 col-sm-1 col-xs-1">
+              <a class="fb-share-button"
+                href="https://www.facebook.com/groups/1419135528352392/"></a>
+        </div>
+        <div class="hidden-md hidden-lg hidden-sm col-xs-1">
+            <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
+            <div class="navbar-header toggle-bar">
+              <button type="button" id="menu_btn" class="navbar-toggle" data-toggle="collapse" data-target="#mobile_menu_items_bar">
+                <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div id="menu_items_bar" class="overlay-container mobile-menu-items collapse navbar-collapse" style="display:none;">
+              <label for="menu_btn"></label>
+              <ul class="nav navbar-main">
+                <li class='active dropdown parent_menu'>
+                  <a href='#' title="home" class='home dropdown-toggle' data-toggle='dropdown'>
+                  <span>Home</span></a>
+                </li>
+                <li class='dropdown parent_menu'>
+                  <a href="#" title="aboutUs" class='aboutUs dropdown-toggle' data-toggle='dropdown'>
+                  <span>About us</span></a>
+                </li>
+                <li class='dropdown parent_menu'>
+                  <a href='#' title="sermons" class='sermons dropdown-toggle' data-toggle='dropdown'>
+                  <span>Sermons</span></a>
+                </li>
+                <li class='dropdown parent_menu'>
+                  <a href='#' title="aboutBrother" class='aboutBrother dropdown-toggle' data-toggle='dropdown'>
+                  <span>About Zac Poonen</span></a>
+                </li>
+                <li class="has-sub dropdown parent_menu">
+                  <a href='#' title="locateUs" class='locateUs dropdown-toggle' data-toggle='dropdown'>
+                  <span>Locate Us</span> <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                  <ul class="dropdown-menu  multi-column-dropdown">
+                    <li class="child_menu menu-list-item">
+                      <a><span>CFC Thirumullaivoyal</span></a>
+                    </li>
+                    <li class="child_menu menu-list-item">
+                      <a><span> CFC Tambaram </span></a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            </nav>
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="menu-nav-container">
   <div class="container">
     <div class="row">
       <div class="col-md-12 hidden-xs">
         <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
           <div id="menu_items_bar" class="collapse navbar-collapse">
-		    <ul class="nav navbar-main">
+        <ul class="nav navbar-main">
               <li class='active dropdown parent_menu'>
                 <a href='#' title="home" class='home dropdown-toggle' data-toggle='dropdown'><span>Home</span></a>
               </li>
@@ -148,61 +150,61 @@ churches in india,churches in chennai, church in india,christian,chrisrtian indi
 <div class="spinner-container" style="display:none;">
 
 </div>
-	<div class="home-page-section">
-	   	<div data-include="templates/home.php"></div>
-	</div>
-	<div class="content-page-section" style="display:none;">
-		<div class="container-fluid body-container bs-docs-container">
-			<div class="row-fluid" id="full-width">
-				<div class="col-md-12 home-page">
-					<div class="col-md-8 content-section" id="content_section">
-					</div>
-					<div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
-					   <div data-include="templates/timings.php"></div>
-					</div>
+  <div class="home-page-section">
+      <div data-include="templates/home.php"></div>
+  </div>
+  <div class="content-page-section" style="display:none;">
+    <div class="container-fluid body-container bs-docs-container">
+      <div class="row-fluid" id="full-width">
+        <div class="col-md-12 home-page">
+          <div class="col-md-8 content-section" id="content_section">
+          </div>
+          <div class="col-md-3 col-sm-12 col-xs-12 timings-section" id="timings_section">
+             <div data-include="templates/timings.php"></div>
+          </div>
         </div>
-			</div>
-		</div>
-	</div>
-	<footer>
-	<div id="footer_container" class="footer-container">
-		<div class="footer-menu">
-			<div class="container footer-area">
-				<div class="row">
-					<div class="col-md-4 col-sm-4 col-xs-12 footer-section-1">
-						<h4>CFC Thirumullaivoyal address</h4>
-						<ul>
-							<li>Plot No. 9 Saraswathi Nagar</li>
-							<li>A.I.R Transmission Towers Compound</li>
-							<li>Thirumullaivoyal</li>
-							<li>Chennai - 600 066</li>
-							<li>Ph : 9444914400, 9840154586</li>
-						</ul>
-					</div>
-					<div class="col-md-4 col-sm-4 col-xs-12 footer-section-2">
-						<h4>CFC Tambaram address</h4>
-						<ul>
-							<li>5/58 First Floor, Ganesh Nagar Main Road</li>
-							<li>Selaiyur Post</li>
-							<li>Chennai - 600 073</li>
-							<li>Ph : 9789019443, 9444119988</li>
-						</ul>
-					</div>
-					<div class="col-md-4 col-sm-4 col-xs-12 footer-section-3">
-						<h4>Recommended Links</h4>
-						<ul>
-						    <li><a href="http://www.cfcindia.com" target="_blank">www.cfcindia.com</a></li>
-							<li><a href="http://rlcfchurch.org" target="_blank">www.rlcfchurch.org</a></li>
-							<li><a href="http://www.newcovenantcf.org/" target="_blank">www.newcovenantcf.org</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="cfcc-bottom-footer">All rights reserved @
-					cfcchennai.church 2016</div>
-			</div>
-		</div>
-	</div>
-	</footer>
+      </div>
+    </div>
+  </div>
+  <footer>
+  <div id="footer_container" class="footer-container">
+    <div class="footer-menu">
+      <div class="container footer-area">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 col-xs-12 footer-section-1">
+            <h4>CFC Thirumullaivoyal address</h4>
+            <ul>
+              <li>Plot No. 9 Saraswathi Nagar</li>
+              <li>A.I.R Transmission Towers Compound</li>
+              <li>Thirumullaivoyal</li>
+              <li>Chennai - 600 066</li>
+              <li>Ph : 9444914400, 9840154586</li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12 footer-section-2">
+            <h4>CFC Tambaram address</h4>
+            <ul>
+              <li>5/58 First Floor, Ganesh Nagar Main Road</li>
+              <li>Selaiyur Post</li>
+              <li>Chennai - 600 073</li>
+              <li>Ph : 9789019443, 9444119988</li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12 footer-section-3">
+            <h4>Recommended Links</h4>
+            <ul>
+                <li><a href="http://www.cfcindia.com" target="_blank">www.cfcindia.com</a></li>
+              <li><a href="http://rlcfchurch.org" target="_blank">www.rlcfchurch.org</a></li>
+              <li><a href="http://www.newcovenantcf.org/" target="_blank">www.newcovenantcf.org</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="cfcc-bottom-footer">All rights reserved @
+          cfcchennai.church 2016</div>
+      </div>
+    </div>
+  </div>
+  </footer>
 <script src="js/lightbox.js"></script>
 </body>
 </html>
