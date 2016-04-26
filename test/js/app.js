15d1a7630c08ea366ea42df87ea5228b41c1e98b
webttp <smartin.black@gmail.com>
2016-04-26 18:12:02 +0530

    Update app.js

--- test/js/app.js
+++ test/js/app.js
@@ -43,3 +43,25 @@ app.controller('customersCrtl', function ($scope, $http, $timeout,$location) {
         $scope.reverse = !$scope.reverse;
     };
 });
+app.controller('imgCrtl', function ($scope, $http, $timeout,$location) {
+	$http.get('getImages.php').success(function(data){
+		$scope.loaded = true;
+		$scope.list = data;
+        $scope.currentPage = 1; //current page
+        $scope.entryLimit = 5; //max no of items to display in a page
+        $scope.filteredItems = $scope.list.length; //Initially for no filter  
+        $scope.totalItems = $scope.list.length;
+    });
+	$scope.setPage = function(pageNo) {
+        $scope.currentPage = pageNo;
+    };
+	$scope.filter = function() {
+        $timeout(function() { 
+            $scope.filteredItems = $scope.filtered.length;
+        }, 10);
+    };
+    $scope.sort_by = function(predicate) {
+        $scope.predicate = predicate;
+        $scope.reverse = !$scope.reverse;
+    };
+});
