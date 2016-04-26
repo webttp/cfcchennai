937773f84e1d64ff19c0362439403e9da13394ca
webttp <smartin.black@gmail.com>
2016-04-27 00:03:22 +0530

    Create footer.php

---
+++ test/templates/footer.php
@@ -0,0 +1,37 @@
+<div id="footer_container" class="footer-container">
+    <div class="footer-menu">
+      <div class="container footer-area">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 col-xs-12 footer-section-1">
+            <h4>CFC Thirumullaivoyal address</h4>
+            <ul>
+              <li>Plot No. 9 Saraswathi Nagar</li>
+              <li>A.I.R Transmission Towers Compound</li>
+              <li>Thirumullaivoyal</li>
+              <li>Chennai - 600 066</li>
+              <li>Ph : 9444914400, 9840154586</li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12 footer-section-2">
+            <h4>CFC Tambaram address</h4>
+            <ul>
+              <li>5/58 First Floor, Ganesh Nagar Main Road</li>
+              <li>Selaiyur Post</li>
+              <li>Chennai - 600 073</li>
+              <li>Ph : 9789019443, 9444119988</li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12 footer-section-3">
+            <h4>Recommended Links</h4>
+            <ul>
+                <li><a href="http://www.cfcindia.com" target="_blank">www.cfcindia.com</a></li>
+              <li><a href="http://rlcfchurch.org" target="_blank">www.rlcfchurch.org</a></li>
+              <li><a href="http://www.newcovenantcf.org/" target="_blank">www.newcovenantcf.org</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="cfcc-bottom-footer">All rights reserved @
+          cfcchennai.church 2016</div>
+      </div>
+    </div>
+  </div>
