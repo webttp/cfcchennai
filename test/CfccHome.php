29d751733933990211fceb5c9e367bd4bfd47575
webttp <smartin.black@gmail.com>
2016-04-27 00:51:41 +0530

    Update CfccHome.php

--- test/CfccHome.php
+++ test/CfccHome.php
@@ -35,7 +35,114 @@
   </head>
   <body class="no-sidebars md">
     <header>
-      <div data-include="templates/menuHeader.php"></div>
+      <div class="menu-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-1 col-sm-1 col-xs-2 cfcc-logo">
+          <a href="http://www.cfcindia.com/"> 
+          <img class="img-responsive main-logo" src="images/Logo.png" alt="Chennai-CFC-Image">
+          </a>
+        </div>
+        <div class="col-md-10 col-sm-10 col-xs-8 cfcc-verse">
+          <div class="col-md-10 col-sm-10 hidden-xs">
+            <a href="http://www.cfcindia.com">
+              <div class="hidden-xs banner-text">Christian Fellowship
+                Church, Chennai</div>
+            </a>
+            <!-- <div class="hidden-xs verse-text">
+                      <em>And he who does not take his cross and follow after Me is not worthy of Me </br> (Matthew 10:38) </em>
+                  </div> -->
+          </div>
+          <div class="hidden-md hidden-lg hidden-sm col-xs-12 xs-banner-text">
+               Christian Fellowship Church, Chennai</div>
+        </div>
+        <div class="col-md-1 col-sm-1 col-xs-1">
+              <a class="fb-share-button"
+                href="https://www.facebook.com/groups/1419135528352392/"></a>
+        </div>
+        <div class="hidden-md hidden-lg hidden-sm col-xs-1">
+            <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
+            <div class="navbar-header toggle-bar">
+              <button type="button" id="menu_btn" class="navbar-toggle" data-toggle="collapse" data-target="#mobile_menu_items_bar">
+                <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div id="menu_items_bar" class="overlay-container mobile-menu-items collapse navbar-collapse" style="display:none;">
+              <label for="menu_btn"></label>
+              <ul class="nav navbar-main">
+                <li class='active dropdown parent_menu'>
+                  <a href='#' title="CfccHome" class='home dropdown-toggle' data-toggle='dropdown'>
+                  <span>Home</span></a>
+                </li>
+                <li class='dropdown parent_menu'>
+                  <a href="#" title="aboutUs" class='aboutUs dropdown-toggle' data-toggle='dropdown'>
+                  <span>About us</span></a>
+                </li>
+                <li class='dropdown parent_menu'>
+                  <a href='#' title="sermons" class='sermons dropdown-toggle' data-toggle='dropdown'>
+                  <span>Sermons</span></a>
+                </li>
+                <li class='dropdown parent_menu'>
+                  <a href='#' title="aboutBrother" class='aboutBrother dropdown-toggle' data-toggle='dropdown'>
+                  <span>About Zac Poonen</span></a>
+                </li>
+                <li class="has-sub dropdown parent_menu">
+                  <a href='#' title="locateUs" class='locateUs dropdown-toggle' data-toggle='dropdown'>
+                  <span>Locate Us</span> <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                  <ul class="dropdown-menu  multi-column-dropdown">
+                    <li class="child_menu menu-list-item">
+                      <a><span>CFC Thirumullaivoyal</span></a>
+                    </li>
+                    <li class="child_menu menu-list-item">
+                      <a><span> CFC Tambaram </span></a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            </nav>
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="menu-nav-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 hidden-xs">
+        <nav class="navbar navbar-default navbar-inverse menu" role="navigation">
+          <div id="menu_items_bar" class="collapse navbar-collapse">
+        <ul class="nav navbar-main">
+              <li class='active dropdown parent_menu'>
+                <a href='#' title="CfccHome" class='home dropdown-toggle' data-toggle='dropdown'><span>Home</span></a>
+              </li>
+              <li class='dropdown parent_menu'>
+                <a href="#" title="aboutUs" class='aboutUs dropdown-toggle' data-toggle='dropdown'><span>About us</span></a>
+              </li>
+              <li class='dropdown parent_menu'>
+                <a href='#' title="sermons"  class='sermons dropdown-toggle' data-toggle='dropdown'><span>Sermons</span></a>
+              </li>
+              <li class='dropdown parent_menu'>
+                <a href='#' title="aboutBrother"  class='aboutBrother dropdown-toggle' data-toggle='dropdown'><span>About Zac Poonen</span></a>
+              </li>
+              <li class="has-sub dropdown parent_menu">
+                <a href='#' title="locateUs"  class='locateUs dropdown-toggle' data-toggle='dropdown'><span>Locate Us</span>
+                  <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                  <ul class="dropdown-menu  multi-column-dropdown">
+                    <li class="child_menu menu-list-item"><a><span> CFC Thirumullaivoyal</span></a></li>
+                    <li class="child_menu menu-list-item"><a><span> CFC Tambaram </span></a></li>
+                  </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</div>
+
     </header>
     <div class="spinner-container" style="display:none;"></div>
     <div class="content-page-section">
