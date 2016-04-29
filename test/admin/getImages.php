1f481c44f3a4858cef32f4395fc41044d04f98ab
webttp <smartin.black@gmail.com>
2016-04-29 12:04:20 +0530

    Update getImages.php

--- test/admin/getImages.php
+++ test/admin/getImages.php
@@ -1,9 +1,11 @@
 <?php
 include("../config/Class.Crud.Php");
 $tablename="imagegallery";
-			$condition = array("");
-			$ordercol="uploadeddate";
+$condition = array("");
+$ordercol="uploadeddate";
 $fetch=$obj->fetchorderby($tablename, $condition,$ordercol);
-print_r($fetch);
-echo "getimwwwwwwwwwwwwwwwwwwwwwwages";
+# JSON-encode the response
+$json_response = json_encode($fetch);
+// # Return the response
+echo $json_response;
 ?>
