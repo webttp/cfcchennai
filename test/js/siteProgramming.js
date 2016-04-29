3a40269b0b54b4830e05f794117a2f6d5a9400eb
webttp <smartin.black@gmail.com>
2016-04-30 01:10:40 +0530

    Update siteProgramming.js

--- test/js/siteProgramming.js
+++ test/js/siteProgramming.js
@@ -10,54 +10,72 @@ $(function(){
 			$(this).addClass("active");
 	});
 	
-	/*$("#menu_items_bar > ul > li.has-sub:hover a").mouseenter(function(e){
-		$("#menu_items_bar > ul > li.has-sub:hover").addClass("menu-item-hover");
-		$("#menu_items_bar > ul > li.has-sub ul").addClass("menu-sub-list-hover");
-	}); */
-	
 	// to load the html pages dynamically using class names
 	$("#menu_items_bar > ul > li a").on("click",function(e){
 		var self = $(this);	
 		var title = self.attr("title");
 		var pageName= "templates/"+ title +".html";
 		if(title == "home"){
-			$(".home-page-section").css("display","block");
-			$(".home-page-section").addClass("lazy");
-			$(".home-page-section").lazy({
-				// loads with a five seconds delay
-				asyncLoader: function(element) {
-					setTimeout(function() {
-						$(".home-page-section").load(pageName);
+			$.ajax({
+				url: pageName,
+				beforeSend: function(xhr){
+					$(".spinner-container").css("display","block");
+				},
+				success: function(result,status,xhr){
+					setTimeout(funtion(){
 						$(".content-page-section").css("display","none");
-					}, 5000);
+						$(".home-page-section").css("display","block");
+						$(".home-page-section").load(result);
+					}, 3000);
+				},
+				error: function(xhr,status,error){
+					$(".error-container").css("display","block");
+					$(".error-container").html("<h3> We are coming soon </h3>");
 				}
+			}).done(function(){
+				$(".spinner-container").css("display","none");
 			});
-				$(".home-page-section").removeClass("lazy");
 		}else {
-			$(".content-page-section").css("display","block");
-			$(".content-section").css("display","block");
-			$(".content-section").addClass("lazy");
-			$(".content-section").lazy({
-				asyncLoader: function(element) {
-					setTimeout(function() {
+			$.ajax({
+				url: pageName,
+				beforeSend: function(xhr){
+					$(".spinner-container").css("display","block");
+				},
+				success: function(result,status,xhr){
+					setTimeout(funtion(){
 						$(".home-page-section").css("display","none");
+						$(".content-page-section").css("display","block");
+						$(".content-section").css("display","block");
 						$(".content-section").html("");
-						$(".content-section").load(pageName);					
-					}, 5000);
+						$(".content-section").load(result);	
+					}, 3000);
+				},
+				error: function(xhr,status,error){
+					$(".error-container").css("display","block");
+					$(".error-container").html("<h3> We are coming soon </h3>");
 				}
+			}).done(function(){
+				$(".spinner-container").css("display","none");
 			});
-			$(".content-section").removeClass("lazy");
+			
 			/* Lazy loadin of timings section */
-			$(".timings-section").addClass("lazy");
-			$(".timings-section").lazy({
-				asyncLoader: function(element) {
-					setTimeout(function() {
-						$(".timings-section").load();
-					}, 5000);
+			$.ajax({
+				url: 'templates/timings.html',
+				beforeSend: function(xhr){
+					$(".spinner-container").css("display","block");
+				},
+				success: function(result,status,xhr){
+					setTimeout(funtion(){
+						$(".timings-section").load(result);
+					}, 3000);
+				},
+				error: function(xhr,status,error){
+					$(".error-container").css("display","block");
+					$(".error-container").html("<h3> We are coming soon </h3>");
 				}
+			}).done(function(){
+				$(".spinner-container").css("display","none");
 			});
-	
-			$(".timings-section").removeClass("lazy");
 		}
 	});
 	
@@ -69,10 +87,7 @@ $(function(){
 	
 	// Menu toggle bar functionalities
 	$(".navbar-toggle").click(function(e){
-		var self = $(this);
-		if(!self.hasClass(".has-sub")){
-			$(".mobile-menu-items").show();
-		}
+		$(".mobile-menu-items").show();
 	});
 	
 	$("#menu_items_bar.mobile-menu-items a").click(function(e){
