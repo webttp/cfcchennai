570ae2ab568a2bbca440c4962a46f830ef730bc9
webttp <smartin.black@gmail.com>
2016-04-29 11:50:38 +0530

    Update getImages.php

--- test/admin/getImages.php
+++ test/admin/getImages.php
@@ -1,12 +1,3 @@
 <?php
-include("../config/Class.Crud.Php");
-$tablename="imagegallery";
-			$userMailid = array("");
-			$fetch=$obj->fetchorderby($tablename, $userMailid);
-			# JSON-encode the response
-			$json_response = json_encode($fetch);
-			// # Return the response
-			echo $json_response;
-
-	
+echo "getimages";
 ?>
