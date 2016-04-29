cb6886131ab83f646942704240cb908fbf77f591
webttp <smartin.black@gmail.com>
2016-04-29 17:36:21 +0530

    Update addImage.php

--- test/admin/addImage.php
+++ test/admin/addImage.php
@@ -42,7 +42,7 @@
 					<label class= "col-md-2"></label>
 					<div class="col-md-4">
 					<button name="submit" class="btn btn-primary" id="upload">Submit</button>
-					<a href="listimages.php" class="btn">Cancel</a> 
+					<a href="listmessages.php" class="btn">Cancel</a> 
 					</div>
 					</div>
 					</form>
@@ -53,41 +53,87 @@
 </html>
 <?php 
 	if(isset($_POST['submit'])!=""){
-		
+
+		function cwUpload($field_name = '', $target_folder = '', $file_name = '', $thumb = FALSE, $thumb_folder = '', $thumb_width = '', $thumb_height = ''){
+
+    //folder path setup
+    $target_path = $target_folder;
+    $thumb_path = $thumb_folder;
+    
+    //file name setup
+    $filename_err = explode(".",$_FILES[$field_name]['name']);
+    $filename_err_count = count($filename_err);
+    $file_ext = $filename_err[$filename_err_count-1];
+    if($file_name != ''){
+        $fileName = $file_name.'.'.$file_ext;
+    }else{
+        $fileName = $_FILES[$field_name]['name'];
+    }
+    
+    //upload image path
+    $upload_image = $target_path.basename($fileName);
+    
+    //upload image
+    if(move_uploaded_file($_FILES[$field_name]['tmp_name'],$upload_image))
+    {
+        //thumbnail creation
+        if($thumb == TRUE)
+        {
+            $thumbnail = $thumb_path.$fileName;
+            list($width,$height) = getimagesize($upload_image);
+            $thumb_create = imagecreatetruecolor($thumb_width,$thumb_height);
+            switch($file_ext){
+                case 'jpg':
+                    $source = imagecreatefromjpeg($upload_image);
+                    break;
+                case 'jpeg':
+                    $source = imagecreatefromjpeg($upload_image);
+                    break;
+
+                case 'png':
+                    $source = imagecreatefrompng($upload_image);
+                    break;
+                case 'gif':
+                    $source = imagecreatefromgif($upload_image);
+                    break;
+                default:
+                    $source = imagecreatefromjpeg($upload_image);
+            }
+
+            imagecopyresized($thumb_create,$source,0,0,0,0,$thumb_width,$thumb_height,$width,$height);
+            switch($file_ext){
+                case 'jpg' || 'jpeg':
+                    imagejpeg($thumb_create,$thumbnail,100);
+                    break;
+                case 'png':
+                    imagepng($thumb_create,$thumbnail,100);
+                    break;
+
+                case 'gif':
+                    imagegif($thumb_create,$thumbnail,100);
+                    break;
+                default:
+                    imagejpeg($thumb_create,$thumbnail,100);
+            }
+
+        }
+
+        return $fileName;
+    }
+    else
+    {
+        return false;
+    }
+}
 				$tablename='imagegallery';
 				$imagename=$_POST['imagename'];
 				$imagealttext=$_POST['imagealttext'];
-				include("imageUpload.php");
-				$upd = new imageUpload("uploadedfile");
-				$upd->setUploadPath("imagebank/");
-				$upd->setThumbPath("imagebank/thumbnail/");
-				$upd->setCreateThumbnail(true);
-				$upd->setThumbDimension(100, 100);
-				$upd->setMaxFileSize(5242880); //in bytes, around 5mb
-				$upd->setThumbMode("crop");
-				echo "uiii";
-				
-				$image_uploaded = $upd->uploadImg();
-				print_r($image_uploaded);
-				echo "aaaaa";
-				exit;
-				
-				if($image_uploaded !== false){     
-				//echo("File uploaded");
-				//proceed database
-				}else{
-				   if($upd->isUploadError()){
-					  //show errors
-					  $msg = $upd->getUploadMsg();
-					  
-				   }else echo("Oops! unknown error");   
-				}  
+				$upload_img = cwUpload('uploadedfile','imagebank/','',TRUE,'imagebank/thumbnail/','200','160');  
 				$imageredirect=$_POST['imageredirect'];
 				$loginid=$_SESSION['loginid'];
 				$currentdt=date('Y-m-d H:i:s',time());
-				$InsColumnVal = array("imagename"=>$imagename,"imgalttext"=>$imagealttext,"imgsrc"=>$image_uploaded,"redirectlink"=>$imageredirect);
-				include("../config/Class.Crud.Php"); 
-			
+				$InsColumnVal = array("imagename"=>$imagename,"imgalttext"=>$imagealttext,"imgsrc"=>$upload_img,"redirectlink"=>$imageredirect,"uploadedby"=>$loginid,"uploadeddate"=>$currentdt);
+				include("../config/Class.Crud.Php"); 	
 				if($obj->insert($tablename, $InsColumnVal)=="New record has been inserted successfully!"){
 					?>
 					<script type="text/javascript">
