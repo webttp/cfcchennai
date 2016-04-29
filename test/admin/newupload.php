80497f6511331df2d6706a9b6fc1cc7836cd8b6c
webttp <smartin.black@gmail.com>
2016-04-29 16:30:54 +0530

    Update newupload.php

--- test/admin/newupload.php
+++ test/admin/newupload.php
@@ -1,97 +1,42 @@
-<?php
-function cwUpload($field_name = '', $target_folder = '', $file_name = '', $thumb = FALSE, $thumb_folder = '', $thumb_width = '', $thumb_height = ''){
-
-    //folder path setup
-    $target_path = $target_folder;
-    $thumb_path = $thumb_folder;
-    
-    //file name setup
-    $filename_err = explode(".",$_FILES[$field_name]['name']);
-    $filename_err_count = count($filename_err);
-    $file_ext = $filename_err[$filename_err_count-1];
-    if($file_name != ''){
-        $fileName = $file_name.'.'.$file_ext;
-    }else{
-        $fileName = $_FILES[$field_name]['name'];
-    }
-    
-    //upload image path
-    $upload_image = $target_path.basename($fileName);
-    
-    //upload image
-    if(move_uploaded_file($_FILES[$field_name]['tmp_name'],$upload_image))
-    {
-        //thumbnail creation
-        if($thumb == TRUE)
-        {
-            $thumbnail = $thumb_path.$fileName;
-            list($width,$height) = getimagesize($upload_image);
-            $thumb_create = imagecreatetruecolor($thumb_width,$thumb_height);
-            switch($file_ext){
-                case 'jpg':
-                    $source = imagecreatefromjpeg($upload_image);
-                    break;
-                case 'jpeg':
-                    $source = imagecreatefromjpeg($upload_image);
-                    break;
-
-                case 'png':
-                    $source = imagecreatefrompng($upload_image);
-                    break;
-                case 'gif':
-                    $source = imagecreatefromgif($upload_image);
-                    break;
-                default:
-                    $source = imagecreatefromjpeg($upload_image);
-            }
-
-            imagecopyresized($thumb_create,$source,0,0,0,0,$thumb_width,$thumb_height,$width,$height);
-            switch($file_ext){
-                case 'jpg' || 'jpeg':
-                    imagejpeg($thumb_create,$thumbnail,100);
-                    break;
-                case 'png':
-                    imagepng($thumb_create,$thumbnail,100);
-                    break;
-
-                case 'gif':
-                    imagegif($thumb_create,$thumbnail,100);
-                    break;
-                default:
-                    imagejpeg($thumb_create,$thumbnail,100);
-            }
-
-        }
-
-        return $fileName;
-    }
-    else
-    {
-        return false;
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
 }
-if(!empty($_FILES['image']['name'])){
-    
-    //call thumbnail creation function and store thumbnail name
-    $upload_img = cwUpload('image','uploads/','',TRUE,'uploads/thumbs/','200','160');
-    
-    //full path of the thumbnail image
-    $thumb_src = 'uploads/thumbs/'.$upload_img;
-    
-    //set success and error messages
-    $message = $upload_img?"<span style='color:#008000;'>Image thumbnail have been created successfully.</span>":"<span style='color:#F00000;'>Some error occurred, please try again.</span>";
-    
-}else{
-    
-    //if form is not submitted, below variable should be blank
-    $thumb_src = '';
-    $message = '';
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
 }
-?>
-<form method="post" action="newupload.php" enctype="multipart/form-data">
-    <input type="file" name="image"/>
-    <input type="submit" name="submit" value="Upload"/>
-</form>
-<?php if($thumb_src != ''){ ?>
-<img src="<?php echo $thumb_src; ?>" alt="">
-<?php } ?>
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
