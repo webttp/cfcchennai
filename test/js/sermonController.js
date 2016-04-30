be754c604d71b38c7de9242074d080cfdc248d0e
webttp <smartin.black@gmail.com>
2016-04-30 23:39:39 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -1,29 +1,8 @@
-$(function(){
-	
-	if($("#menu_items_bar .has-sub ul li").hasClass(".has-sub")){
-		$("#menu_items_bar .has-sub .has-sub").append("<i class='fa-caret-right'></i>");
-	}
-	
-	// To add the active class to menu items
-	$("#menu_items_bar > ul > li a").mouseenter(function(e){
-			$("#menu_items_bar > ul > li.active").removeClass("active");
-			$(this).addClass("active");
-	});
-	
-	// to load the html pages dynamically using class names
-	$("#menu_items_bar > ul > li a").on("click",function(e){
-		var self = $(this);	
-		var title = self.attr("title");
-		var pageName= "templates/"+ title +".html";
-		if(title == "home"){
-			$(".content-page-section").css("display","none");
-			$(".home-page-section").css("display","block");
-			$(".home-page-section").load(pageName);
-		}else {
-			$(".home-page-section").css("display","none");
-			$(".content-page-section").css("display","block");
-			$(".content-section").html("");
-			$(".content-section").load(pageName);
-		}
-	});
+// Create your app with 'youtube-embed' dependency
+var myApp = angular.module('myApp', ['mediaPlayer']);
+
+// Inside your controller...
+myApp.controller('sermonController', function ($scope) {
+  // have a video id
+  $scope.videoId = 'sMKoNBRZM1M';
 });
