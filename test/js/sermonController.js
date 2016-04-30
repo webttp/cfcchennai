bdbf99378c34211646a3b9770691111bad578561
webttp <smartin.black@gmail.com>
2016-04-30 23:45:26 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -2,7 +2,7 @@
 var sermonApp = angular.module('myApp', ['mediaPlayer']);
 
 // Inside your controller...
-myApp.controller('sermonController', function ($scope) {
+sermonApp.controller('sermonController', function ($scope) {
   // have a video id
   $scope.videoId = 'sMKoNBRZM1M';
 });
