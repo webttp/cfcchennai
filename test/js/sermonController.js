aa6cb1a8462f0d84974ca4d86ff047898db5e8ba
webttp <smartin.black@gmail.com>
2016-05-02 00:52:31 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -1,8 +1,81 @@
-// Create your app with 'mediaplayer' dependency
-var sermonApp = angular.module('myApp', ['mediaPlayer']);
+// Create your app with 'videogular' dependency
+'use strict';
+var sermonApp = angular.module('myApp', [ "ngSanitize", "com.2fdevs.videogular", 
+			"com.2fdevs.videogular.plugins.controls",
+            "com.2fdevs.videogular.plugins.overlayplay",
+            "com.2fdevs.videogular.plugins.poster",
+            "com.2fdevs.videogular.plugins.buffering"]);
+    
+	sermonApp.controller('sermonController',["$sce",'$scope', '$routeParams', '$http', function ($sce,scope,routeParams,http) {
+         
+		$http.get('messagesData.json').success(function(data) {
+			$scope.messages = data;
+		}); 
+		
+		this.config = {
+                sources: [
+              {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.mp3"), type: "audio/mpeg"},
+              {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.ogg"), type: "audio/ogg"}
+          ],
+        theme: {
+          url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+                }
+            };
+		    
+			var controller = this;
+            controller.state = null;
+            controller.API = null;
+            controller.currentVideo = 0;
 
-// Inside your controller...
-sermonApp.controller('sermonController', function ($scope) {
-  // have a video id
-  $scope.videoId = 'sMKoNBRZM1M';
-});
+            controller.onPlayerReady = function(API) {
+                controller.API = API;
+            };
+
+            controller.onCompleteVideo = function() {
+                controller.isCompleted = true;
+
+                controller.currentVideo++;
+
+                if (controller.currentVideo >= controller.videos.length) controller.currentVideo = 0;
+
+                controller.setVideo(controller.currentVideo);
+            };
+
+            controller.videos = [
+            {
+                sources: [
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+                ]
+            },
+            {
+                sources: [
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov"), type: "video/mp4"},
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/big_buck_bunny_720p_stereo.ogg"), type: "video/ogg"}
+                ]
+            }
+        ];
+
+            controller.config = {
+                preload: "none",
+                autoHide: false,
+                autoHideTime: 3000,
+                autoPlay: false,
+                sources: controller.videos[0].sources,
+                theme: {
+                    url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+                },
+                plugins: {
+                    poster: "http://www.videogular.com/assets/images/videogular.png"
+                }
+            };
+
+            controller.setVideo = function(index) {
+                controller.API.stop();
+                controller.currentVideo = index;
+                controller.config.sources = controller.videos[index].sources;
+                $timeout(controller.API.play.bind(controller.API), 100);
+            };
+        }]
+ );	
