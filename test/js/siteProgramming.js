056caaa1c89dc3cdce2844c04fbccfc9128430b3
webttp <smartin.black@gmail.com>
2016-05-04 01:15:37 +0530

    Update siteProgramming.js

--- test/js/siteProgramming.js
+++ test/js/siteProgramming.js
@@ -22,65 +22,7 @@ $(function(){
 		var newpath = path + url;
 		location.assign(newpath);
 	});
-	/*
-        // to load the html pages dynamically using class names
-	$("#menu_items_bar > ul > li a").on("click",function(e){
-		var self = $(this);	
-		var title = self.attr("title");
-		var pageName= "templates/"+ title +".html";
-		var timingsPage = "/templates/timings.html";
-		var contactPage;
-		 if(title == "cfcc-thirumullai"){
-		 	contactPage = "/templates/thirumullai-contact.html";
-		 }else if(title == "cfcc-tambaram"){
-		 	contactPage = "/templates/tambaram-contact.html";
-		 }
-		if(title == "home"){
-			$(".content-page-section").css("display","none");
-			$(".home-page-section").css("display","block");
-			$(".home-page-section").load(pageName);
-		}else if(title == "cfcc-thirumullai" || title == "cfcc-tambaram"){
-			
-			$(".home-page-section").css("display","none");
-			$(".timings-section").css("display","none");
-			$(".content-section").html("");
-			$(".content-section").removeClass("content-border");
-			$(".timings-section").html("");
-			$(".content-page-section").css("display","block");
-			$(".contacts-section").css("display","block");
-			$(".loading-container").css("display","block");
-			setTimeout(function(){
-				$(".content-section").load(pageName);
-				$(".loading-container").css("display","none");
-				$(".content-section").addClass("content-border");
-	        	}, 1000);
-			setTimeout(function(){
-				$(".contacts-section").load(contactPage);
-			}, 2000);
-		}
-		else {
-			$(".home-page-section").css("display","none");
-			$(".contacts-section").css("display","none");
-			$(".contacts-section").html("");
-			$(".content-section").html("");
-			$(".content-section").removeClass("content-border");
-			$(".timings-section").html("");
-			$(".content-page-section").css("display","block");
-			$(".loading-container").css("display","block");
-			$(".timings-section").css("display","block");
-			setTimeout(function(){
-				$(".content-section").load(pageName);
-				$(".loading-container").css("display","none");
-				$(".content-section").addClass("content-border");
-			
-	        	}, 1000);
-			setTimeout(function(){
-				$(".timings-section").load(timingsPage);
-			}, 2000);
-			
-		}
-	}); 
-	*/
+	
 	lightbox.option({
 	      'resizeDuration': 200,
 	      'left': '300px',
