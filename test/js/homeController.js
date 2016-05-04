6f81038b02bcd833af65993ea759c3c83f3a9fd2
webttp <smartin.black@gmail.com>
2016-05-04 12:11:13 +0530

    Create homeController.js

---
+++ test/js/homeController.js
@@ -0,0 +1,6 @@
+ angular.module('myApp', []).controller('MyCtrl',MyCtrl);
+ function MyCtrl($scope, $http, $timeout) {
+		$http.get('/admin/getImages.php').success(function(data){
+		$scope.items = data;
+    });
+}
