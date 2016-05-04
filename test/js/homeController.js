5e88ec8c7cc25a6be34045c96c98701205d164c4
webttp <smartin.black@gmail.com>
2016-05-04 12:44:06 +0530

    Update homeController.js

--- test/js/homeController.js
+++ test/js/homeController.js
@@ -1,4 +1,4 @@
- var homeApp = angular.module('myApp', []).controller('MyCtrl',MyCtrl);
+ var homeApp = angular.module('myApp', ['ngRoute']).controller('MyCtrl',MyCtrl);
  function MyCtrl($scope, $http, $timeout) {
 		$http.get('../admin/getImages.php').success(function(data){
 		$scope.items = data;
