75b3f48c76bf935c896da13247b3733f33ba5806
webttp <smartin.black@gmail.com>
2016-05-04 14:46:02 +0530

    hgjhg
    dcdasdsad

--- test/js/homeController.js
+++ test/js/homeController.js
@@ -3,7 +3,7 @@
  myApp.controller('MyCtrl',['$scope','$http','$timeout',myController]);
  
  function myController($scope, $http, $timeout){
- 	$http.get('../../admin/getImages.php').success(function(data){
+ 	$http.get('../admin/getImages.php').success(function(data){
 		$scope.items = data;
  	});
  }
