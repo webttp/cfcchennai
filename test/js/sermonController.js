b88ada5c9d2ad4b1c67e0fc47bd360e9bb1da139
webttp <smartin.black@gmail.com>
2016-05-04 12:44:52 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -1,6 +1,6 @@
 // Create your app with 'videogular' dependency
 'use strict';
-var sermonApp = angular.module('sermonApp', [ "ngSanitize", "com.2fdevs.videogular", 
+var sermonApp = angular.module('sermonApp', [ "ngRoute","ngSanitize", "com.2fdevs.videogular", 
 			"com.2fdevs.videogular.plugins.controls",
             "com.2fdevs.videogular.plugins.overlayplay",
             "com.2fdevs.videogular.plugins.poster",
