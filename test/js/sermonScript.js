cd3b2ce508853cf75023e1a0fa8447a47ace395d
webttp <smartin.black@gmail.com>
2016-05-11 02:03:58 +0530

    Update sermonScript.js

--- test/js/sermonScript.js
+++ test/js/sermonScript.js
@@ -11,56 +11,9 @@ $(document).ready(function() {
 		if(item.hasClass("sermon-video")){
 			$(".sermons-player-section .audio-section").css("display","none");
 			$(".sermons-player-section .video-section").css("display","block");
-			changeVideo(src); 
+			$(".video-section .player iframe").attr("src",src);
 		} 
 	});
 });  
 
 
-function loadVideoOnLoad(src) {
-	var tag = document.createElement('script');
-
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-	// 3. This function creates an <iframe> (and YouTube player)
-	// after the API code downloads.
-	
-	function onYouTubeIframeAPIReady() {
-		player = new YT.Player(
-				'player',
-				{
-					height : '340',
-					width : '720',
-					videoId : "http://www.youtube.com/embed/MslSfF9AewQ?rel=0&showinfo=0&enablejsapi=1",
-					events : {
-						'onReady' : onPlayerReady,
-						'onStateChange' : onPlayerStateChange
-					}
-				});
-	}
-
-}
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {	
-  event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
- 
-function changeVideo(videoId){
-	player.loadVideoById({videoId:videoId, startSeconds:5, endSeconds:60, suggestedQuality:'large'});
-}
