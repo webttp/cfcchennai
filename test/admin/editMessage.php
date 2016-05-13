9e7a2eda6efd27286a5ffdab51994315b25df765
webttp <smartin.black@gmail.com>
2016-05-13 14:27:19 +0530

    added origin youtube

--- test/admin/editMessage.php
+++ test/admin/editMessage.php
@@ -132,7 +132,7 @@ if(isset($_POST['submit'])!=""){
 		}	
 		$condition = array("id"=>$msgid);
 		$set = array("date"=>$date,"location"=>$location,"title"=>$title,"speaker"=>$speaker,
-					 "messagetype"=>$messagetype,"videokey"=>$videokey,"audiokey"=>$audiokey,
+					 "messagetype"=>$messagetype,"videokey"=>$videokey."?enablejsapi=1&origin=http://cfcchennai.church","audiokey"=>$audiokey,
 					 "modifiedby"=>$loginid,"modifieddate"=>$currentdt);	
 		$tablename='messagelist';
 		$msg=$obj->update($tablename, $set,$condition);
