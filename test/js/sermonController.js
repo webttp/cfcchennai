67f0b5e5ade5712a25a3dc5fbeddda406176f672
webttp <smartin.black@gmail.com>
2016-05-13 21:29:28 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -1,11 +1,15 @@
 var sermonApp = angular.module('sermonApp', []);
 
-sermonApp.controller('sermonController',MyCtrl);
-
-function MyCtrl($scope, $http, $timeout) {
-   		$http.get('admin/getMessages.php?action=start').success(function(data){
+sermonApp.controller('sermonController',function($scope, $http, $timeout){
+   $http.get('admin/getMessages.php?action=start').success(function(data){
    		$scope.loaded = true;
    		$scope.messages = data;
-       });
-      
-   }
+   });
+   $scope.locationItems = [
+      {  id: 1,
+         location: 'CFC-Tambaram'},
+      {  id: 2,
+         location: 'CFC-Thirumullaivoyal' }
+      ]
+});
+
