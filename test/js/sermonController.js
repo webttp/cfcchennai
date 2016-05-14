6fbb7392eb2bfc1b6b4d694d7076090c17994b37
webttp <smartin.black@gmail.com>
2016-05-14 16:12:51 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -3,7 +3,7 @@ var sermonApp = angular.module('sermonApp', ['angular-toArrayFilter']);
 sermonApp.controller('sermonController',function($scope, $http, $timeout){
    $http.get('admin/getMessages.php?action=start').success(function(data){
    		$scope.loaded = true;
-   		$scope.messages = JSON.stringify(data);
+   		$scope.messages = JSON.parse(data);
    });
    
 });
