9853f070ab4d3c8639ebd1938f8e1ddc0334d8d6
webttp <smartin.black@gmail.com>
2016-05-15 13:45:38 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -22,55 +22,13 @@ sermonApp.controller('sermonController',function($scope, $http, $timeout){
     
     $scope.showData = function( ){
 
-	 $scope.itemsPerPage = 5;
-	 $scope.currentPage = 1;
-	 $scope.range = function() {
-	    var rangeSize = 4;
-	    var ps = [];
-	    var start;
-	
-	    start = $scope.currentPage;
-	    if ( start > $scope.pageCount()-rangeSize ) {
-	      start = $scope.pageCount()-rangeSize+1;
-	    }
-	     else {
-			 start = $scope.pageCount() + 1;
-		 }
-	    for (var i=start; i<start+rangeSize; i++) {
-	      ps.push(i);
-	    }
-	    return ps;
-	  };
-	
-	  $scope.prevPage = function() {
-	    if ($scope.currentPage > 0) {
-	      $scope.currentPage--;
-	    }
-	  };
-	
-	  $scope.DisablePrevPage = function() {
-	    return $scope.currentPage === 0 ? "disabled" : "";
-	  };
-	
-	  $scope.pageCount = function() {
-	    return Math.ceil($scope.messages.length/$scope.itemsPerPage)-1;
-	  };
-	
-	  $scope.nextPage = function() {
-	    if ($scope.currentPage < $scope.pageCount()) {
-	      $scope.currentPage++;
-	    }
-	  };
-	
-	  $scope.DisableNextPage = function() {
-	    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
-	  };
-	
-	  $scope.setPage = function(n) {
-	    $scope.currentPage = n;
-	  };
-	    
-	}
+	 $scope.curPage = 0;
+ 	$scope.pageSize = 5;
+ 
+  	$scope.numberOfPages = function() {
+				return Math.ceil($scope.messages.length / $scope.pageSize);
+	};
+    }
 });
 
 angular.module('sermonApp').filter('pagination', function()
