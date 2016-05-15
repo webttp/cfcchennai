7b8ce2bf6cafe76ec6862015c1cf13f3ee83b27a
webttp <smartin.black@gmail.com>
2016-05-15 12:36:56 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -1,14 +1,18 @@
 var sermonApp = angular.module('sermonApp', ['angular-toArrayFilter', '720kb.datepicker']);
 
 sermonApp.controller('sermonController',function($scope, $http, $timeout){
+   // setting the number of records per page
+   $scope.pageLimit = 5;
    $http.get('admin/getMessages.php?action=start').success(function(data){
    		$scope.loaded = true;
    		$scope.messages = data;
+   		$scope.pageLimit = 5; // setting the number of records per page
    });
    
    $scope.locationtrigger = function(place) {
 			$http.get('admin/getMessages.php?action='+'locate&'+'location='+place).success(function(data){
 			$scope.messages = data;
+			$scope.pageLimit = 5; // setting the number of records per page
 	});
    }
    $scope.sort_by = function(predicate) {
