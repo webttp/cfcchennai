fa26f0cd2a6cf2de3da41ae44d547509a656f2eb
webttp <smartin.black@gmail.com>
2016-05-15 13:15:41 +0530

    Update sermonController.js

--- test/js/sermonController.js
+++ test/js/sermonController.js
@@ -2,7 +2,7 @@ var sermonApp = angular.module('sermonApp', ['angular-toArrayFilter', '720kb.dat
 
 sermonApp.controller('sermonController',function($scope, $http, $timeout){
    // setting the number of records per page
-   $scope.pageLimit = 5;
+   
    $http.get('admin/getMessages.php?action=start').success(function(data){
    		$scope.loaded = true;
    		$scope.messages = data;
@@ -19,5 +19,63 @@ sermonApp.controller('sermonController',function($scope, $http, $timeout){
         $scope.predicate = predicate;
         $scope.reverse = !$scope.reverse;
     };
+    
+    $scope.showData = function( ){
+
+	 $scope.itemsPerPage = 5;
+	 $scope.currentPage = 0;
+		
+	 $scope.range = function() {
+		var rangeSize = 4;
+		var ps = [];
+		var start;
+
+		start = $scope.currentPage;
+		if ( start > $scope.pageCount()-rangeSize ) {
+		  start = $scope.pageCount()-rangeSize+1;
+		}
+
+		for (var i=start; i<start+rangeSize; i++) {
+		  ps.push(i);
+		}
+		return ps;
+	  };
+
+	  $scope.prevPage = function() {
+		if ($scope.currentPage > 0) {
+		  $scope.currentPage--;
+		}
+	  };
+
+	  $scope.DisablePrevPage = function() {
+		return $scope.currentPage === 0 ? "disabled" : "";
+	  };
+
+	  $scope.pageCount = function() {
+		return Math.ceil($scope.datalists.length/$scope.itemsPerPage)-1;
+	  };
+
+	  $scope.nextPage = function() {
+		if ($scope.currentPage < $scope.pageCount()) {
+		  $scope.currentPage++;
+		}
+	  };
+
+	  $scope.DisableNextPage = function() {
+		return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+	  };
+
+	  $scope.setPage = function(n) {
+		$scope.currentPage = n;
+	  };
+         
+	}
 });
 
+angular.module('sermonApp').filter('pagination', function()
+{
+  return function(input, start) {
+    start = parseInt(start, 10);
+    return input.slice(start);
+  };
+});
