24c93bbafcbb32e99bec3bab254283d9f1d652d0
webttp <smartin.black@gmail.com>
2016-05-16 23:39:02 +0530

    Update sermonProgramming.js

--- test/js/sermonProgramming.js
+++ test/js/sermonProgramming.js
@@ -7,66 +7,18 @@ $(document).ready(function() {
 		$(".home-page-spinner").css("display","none");
 		$(".loading-spinner").css("display","none");},2000);
 	
-/*	var mediaPlayer = $('audio').mediaelementplayer({
-			// if the <video width> is not specified, this is the default
-		    defaultVideoWidth: 480,
-		    // if the <video height> is not specified, this is the default
-		    defaultVideoHeight: 270,
-		    // if set, overrides <video width>
-		    videoWidth: -1,
-		    // if set, overrides <video height>
-		    videoHeight: -1,
-		    // width of audio player
-		    audioWidth: 400,
-		    // height of audio player
-		    audioHeight: 30,
-		    // initial volume when the player starts
-		    startVolume: 0.8,
-		    // useful for <audio> player loops
-		    loop: false,
-		    // enables Flash and Silverlight to resize to content size
-		    enableAutosize: true,
-		    // the order of controls you want on the control bar (and other plugins below)
-		    features: ['playpause','progress','current','duration','tracks','volume','fullscreen'],
-		    // Hide controls when playing and mouse is not over the video
-		    alwaysShowControls: false,
-		    // force iPad's native controls
-		    iPadUseNativeControls: false,
-		    // force iPhone's native controls
-		    iPhoneUseNativeControls: false, 
-		    // force Android's native controls
-		    AndroidUseNativeControls: false,
-		    // forces the hour marker (##:00:00)
-		    alwaysShowHours: false,
-		    // show framecount in timecode (##:00:00:00)
-		    showTimecodeFrameCount: false,
-		    // used when showTimecodeFrameCount is set to true
-		    framesPerSecond: 25,
-		    // turns keyboard support on and off for this instance
-		    enableKeyboard: true,
-		    // when this player starts, it will pause other players
-		    pauseOtherPlayers: true,
-		    // array of keyboard commands
-		    keyActions: []
-	});
-	*/
 	// To handle the click event of music , video and download buttons
 	$(".sermon-content").on("click","a",function(e){
 		var item = $(this);
 		if(item.attr("id") != "sermon-download"){
 			e.preventDefault();
-			/* var player = new MediaElementPlayer('audio');
-			player.pause();*/
 			var title = item.attr("title");
 			$(".sermon-section-title").html(title);
 			var src =  item.attr("class");
 			if(item.attr("id") == "sermon-audio"){
 				$(".sermons-player-section .video-section").css("display","none");
 				$(".sermons-player-section .audio-section").css("display","block");
-				updatePlayer(title,src);
-				/*player.setSrc(src) ;
-				player.load();
-				player.play();*/
+				updateAudioPlayer(title,src);
 				$("html, body").animate({ scrollTop: 0 }, "slow");
 			} else if(item.attr("id") == "sermon-video"){
 				$(".sermons-player-section .audio-section").css("display","none");
@@ -77,7 +29,7 @@ $(document).ready(function() {
 		}
 	});
 	  
-	function updatePlayer(title, src){
+	function updateAudioPlayer(title, src){
 	        var player = $("#audio_jplayer_1");
 	        player.jPlayer("destroy");
 	        player.jPlayer({
@@ -99,6 +51,7 @@ $(document).ready(function() {
 	         toggleDuration: true
 	       });
 	}
+
 	/* var recordsPerPage = 5;
 	var totalNumRecords;
 	// Loading of the Sermon table contents
