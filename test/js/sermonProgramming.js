d91ea8dee719e08ba44be9e0eed57e2d7e396b65
webttp <smartin.black@gmail.com>
2016-05-22 17:48:59 +0530

    Update sermonProgramming.js

--- test/js/sermonProgramming.js
+++ test/js/sermonProgramming.js
@@ -26,11 +26,13 @@ $(document).ready(function() {
 			$(".sermon-section-title").html(title);
 			var src =  item.attr("class");
 			if(item.attr("id") == "sermon-audio"){
+				$(".video-section .player iframe").attr("src",null);
 				$(".sermons-player-section .video-section").css("display","none");
 				$(".sermons-player-section .audio-section").css("display","block");
 				updateAudioPlayer(title,src);
 				$("html, body").animate({ scrollTop: 0 }, "slow");
 			} else if(item.attr("id") == "sermon-video"){
+				$("#audio_jplayer_1").jPlayer( "stop" );
 				$(".sermons-player-section .audio-section").css("display","none");
 				$(".sermons-player-section .video-section").css("display","block");
 				$(".video-section .player iframe").attr("src",src);
@@ -69,42 +71,33 @@ $(document).ready(function() {
 	$(".pagination").hide();
 	var recordsPerPage = 5;
 	var totalNumRecords = $(".sermon-content .sermon-row").length;
-
 	//recordsPerPage is the number of items you want to display on each page
 	//totalNumRecords is the total number of items that you have
    if( recordsPerPage < totalNumRecords) {
 	 //Show the pagination controls
 	    $(".pagination").show();
-
 	    //loop through all of the divs and hide them by default.
 	    for (var i=0; i <= totalNumRecords; i++) {
 	        $(".sermon-content").find(".sermon-row.index" + i).hide();
 	    }
-
 	    //then only display the number of divs the user dictated
 	    for (var i = 0; i < recordsPerPage; i++) {
 	        $(".sermon-content").find(".sermon-row.index" + i).show();
 	    }
-
 	    //maxPages is the maximum amount of pages needed for pagination. (round up) 
 	    var maxPages = Math.ceil(totalNumRecords/recordsPerPage);   
-
 	    $('.pagination').jqPagination({
 	        link_string : '/?{page_number}',
 	        max_page     : maxPages,
 	        paged        : function(page) { 
-
 	            //a new page has been requested
-
 	            //loop through all of the divs and hide them all.
 	            for (var i=0; i < totalNumRecords; i++) {
 	            	 $(".sermon-content").find(".sermon-row.index" + i).hide();
 	            }
-
 	            //Find the range of the records for the page: 
 	            var recordsFrom = recordsPerPage * (page-1);
 	            var recordsTo = recordsPerPage * (page);
-
 	            //then display only the records on the specified page
 	            for (var i = recordsFrom; i < recordsTo; i++) {
             	$(".sermon-content").find(".sermon-row.index" + i).show();
@@ -131,4 +124,3 @@ $(document).ready(function() {
 	});
 
 });
-
